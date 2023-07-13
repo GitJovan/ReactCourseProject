d3277d76a50ad09131b1543eaeedcb2d67b31b6a
coja <djordjevic.d.jovan@gmail.com>
2023-07-13 16:30:02 +0200

    beautify

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -2,19 +2,19 @@ import './Expenses.css'
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from '../expensesFilter/ExpensesFilter';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 export default function Expenses({ expensesData }) {
-  const [selectedDateFilter, setSelectedDateFilter] = useState('');
+  const [selectedYearFilter, setSelectedYearFilter] = useState('2020');
 
-  const filterChangeHandler = (date) => {
-    setSelectedDateFilter(date);
+  const filterChangeHandler = (year) => {
+    setSelectedYearFilter(year);
   }
 
   return (
     <div>
       <Card className="expenses">
-        <ExpensesFilter onFilterChange={filterChangeHandler} />
+        <ExpensesFilter selected={selectedYearFilter} onFilterChange={filterChangeHandler} />
         <ExpenseItem title={expensesData[0].title} amount={expensesData[0].amount} date={expensesData[0].date}></ExpenseItem>
         <ExpenseItem title={expensesData[1].title} amount={expensesData[1].amount} date={expensesData[1].date}></ExpenseItem>
         <ExpenseItem title={expensesData[2].title} amount={expensesData[2].amount} date={expensesData[2].date}></ExpenseItem>
